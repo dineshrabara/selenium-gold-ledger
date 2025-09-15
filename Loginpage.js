5791592cc257a848d25ab874e62afaf8dd4c4d6d
Jenish Varsani <jenishvarsani9099@gmail.com>
2025-09-15 16:27:41 +0530

    Login page test case change and login page test case added

---
+++ Loginpage.js
@@ -0,0 +1,36 @@
+Feature('login');
+
+Scenario('Login Page', ({ I }) => {
+  I.amOnPage('/login');
+
+// Wrong login user & password
+  I.fillField('username', 'hello@hello.com');
+  I.fillField('password', 'Hello@1234');
+  I.click('Sign In');
+  I.waitForText('The user credentials were incorrect.', 5)
+  I.see('The user credentials were incorrect.')
+
+//   Correct login user & password
+  I.fillField('username', process.env.USER);
+  I.fillField('password', process.env.PASSWORD);
+  I.click('Sign In');
+  I.waitForText('Login Completed Successfully', 10)
+  I.see('Login Completed Successfully')
+
+//   Dashboard verify
+  I.waitForText('Welcome back, Jenish Varsani! 👋', 10);
+  I.see('Welcome back, Jenish Varsani! 👋');
+  I.see('Total Revenue');
+  I.see('Net Profit / Loss');
+  I.see('Pending Receipts');
+  I.see('Pending Payments');
+  I.see('Cash Flow');
+  I.see('Working Capital');
+  I.see('Total Assets');
+  I.see('Profit Margin');
+  I.see('Bank & Cash Distribution');
+  I.see('Expense Breakdown');
+  I.see('Top 5 Customers');
+  I.see('Top 5 Suppliers');
+
+});
--- steps_file.js
+++ steps_file.js
@@ -1,10 +1,20 @@
 // in this file you can append custom step methods to 'I' object
 
+// Define custom steps here, use 'this' to access default methods of I.
+// It is recommended to place a general 'login' function here.
+
 module.exports = function() {
   return actor({
-
-    // Define custom steps here, use 'this' to access default methods of I.
-    // It is recommended to place a general 'login' function here.
-
+    login: function(
+      username = process.env.USER,
+      password = process.env.PASSWORD
+    )
+    {
+      this.amOnPage('/login');
+      this.fillField('username', username);
+      this.fillField('password', password);
+      this.click('Sign In');
+      this.waitForNavigation();
+    }
   });
 }
